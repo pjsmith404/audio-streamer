cba1a9158a7ff0aa09f3201d8de9654f18df6643
pjsmith404 <pjsmith404@gmail.com>
2026-06-26 22:44:45 +1000

    Browsing of the file list in a div, along with some other nonsense

--- app/app.js
+++ app/app.js
@@ -1,43 +1,58 @@
-const CONTENT_ROOT = "../content/"
+const CONTENT_ROOT = "http://localhost:8080/content/"
 let currentPath = CONTENT_ROOT;
 
 document.addEventListener('DOMContentLoaded', async () => {
 	document.getElementById('file-list').innerHTML = await getData(CONTENT_ROOT);
 });
 
-document.getElementById('file-list').addEventListener('click', async function(event) {
+const fileList = document.getElementById('file-list');
+fileList.addEventListener('click', loadFileList);
+
+const songQueue = document.getElementById("song-queue");
+
+const audioPlayer = document.getElementById('audio-player');
+
+async function loadFileList(event) {
 	event.preventDefault();
 
 	const targetLink = event.target.closest('a');
-	console.log(targetLink);
 	const targetPath = targetLink.getAttribute('href');
-	const newPath = currentPath + targetPath;
-	console.log(newPath);
-	console.log(currentPath);
+	const nextPath = currentPath + targetPath;
 
-	const newHtml = await getData(newPath);
-
+	const newHtml = await getData(nextPath);
 	const parser = new DOMParser();
 	const newDoc = parser.parseFromString(newHtml, 'text/html');
-	const modDoc = newDoc.body.innerHTML + `<pre><a href="../">..</a></pre>`;
+	console.log(newDoc.body.children);
+
+	const modDoc = `<pre><a href="../">..</a></pre>` + newDoc.body.innerHTML;
 
 	document.getElementById('file-list').innerHTML = modDoc;
 
-	currentPath = newPath;
-	currentPath = trimPath(currentPath);
-});
+	currentPath = nextPath;
+}
 
 async function getData(targetUrl) {
 	const response = await fetch(targetUrl);
-	text = await response.text();
-	return text;
-};
+	console.log(response);
+	const contentType = response.headers.get("content-type");
+	console.log(contentType);
+	if (contentType.includes('text/html')) {
+		text = await response.text();
+		return text;
+	}
+
+	return 'Boooooo';
+}
 
 function trimPath(path) {
-	splitPath = path.split('/');
-	if (splitPath.slice(-1) === '..') {
-		splitPath.pop()
-		splitPath.pop()
+	const splitPath = path.split('/');
+	console.log(splitPath);
+	splitPath.pop();
+	console.log(splitPath);
+	if (splitPath[-1] === '..') {
+		splitPath.pop();
+		splitPath.pop();
+		console.log(splitPath);
 		const trimmedPath = splitPath.join('/');
 
 		const finalPath = trimmedPath + '/';

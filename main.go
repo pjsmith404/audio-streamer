15e54f71d3f00a39ff16b6fec29b31ce16bd7a76
pjsmith404 <pjsmith404@gmail.com>
2026-07-01 19:54:40 +1000

    A few touch ups to the content serving
    - Set the content path with an env var
    - Remove caching on the content path for now just to help with testing
    - Serve on the local network by default

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"os"
 )
 
 func main() {
+	contentPath := os.Getenv("CONTENT_PATH")
+	if contentPath == "" {
+		log.Fatal("CONTENT_PATH not set")
+	}
+
 	mux := http.NewServeMux()
 
 	mux.Handle("/{$}", http.RedirectHandler("/app/", 301))
@@ -14,11 +20,11 @@ func main() {
 	appHandler := http.StripPrefix("/app/", http.FileServer(http.Dir("./app")))
 	mux.Handle("/app/", appHandler)
 
-	contentHandler := http.StripPrefix("/content/", http.FileServer(http.Dir("./content")))
-	mux.Handle("/content/", contentHandler)
+	contentHandler := http.StripPrefix("/content/", http.FileServer(http.Dir(contentPath)))
+	mux.Handle("/content/", noCacheMiddleware(contentHandler))
 
 	s := &http.Server{
-		Addr: ":8080",
+		Addr: "0.0.0.0:8080",
 		Handler: mux,
 	}
 
